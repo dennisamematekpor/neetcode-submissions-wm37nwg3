052685e6fc0c6a5fe9d9c9f7acdfb071b8b32b7f
dennisamematekpor ANANE <108346201+dennisamematekpor@users.noreply.github.com>
2026-07-15 14:54:01 +0200

    Add: top-k-elements-in-list - submission-79

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-79.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Get the numbers in order by first getting the frequency of each number 
+        frequency = defaultdict(list)
+        count = defaultdict(int)
+
+        for num in nums:
+            count[num] += 1
+
+        for num, count in count.items():
+            frequency[count].append(num)
+
+        result = []
+        for i in range(len(nums), 0, -1):  # Count in reverse 
+            for val in frequency[i]: # Frequency of the numbers in reverse direction 
+                result.append(val)  
+                if len(result) == k:
+                    return result
+
+        return []
